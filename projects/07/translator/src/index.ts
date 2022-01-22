7d5730c6299ed939c07c0a660d00506ae4734b75
modderme123 <modderme123@gmail.com>
2022-01-22 12:57:24 -0800

    refactor more

--- projects/07/translator/src/index.ts
+++ projects/07/translator/src/index.ts
@@ -31,6 +31,16 @@ const inlineTable = {
 };
 
 const output = [];
+function write(strings: TemplateStringsArray, ...values: Array<string | number>) {
+  let result = "";
+  for (let i = 0; i < strings.length; i++) {
+    result += strings[i];
+    if (i < values.length) {
+      result += values[i];
+    }
+  }
+  output.push(result.replace(/^\s+/gm, ""));
+}
 
 function writePush(location: string, offset: string) {
   if (!["constant", "local", "argument", "this", "that", "temp", "pointer", "static"].includes(location)) {
@@ -38,25 +48,25 @@ function writePush(location: string, offset: string) {
   }
 
   if (location == "pointer" || location == "temp") {
-    output.push("@" + inlineTable[location + offset]);
-    output.push("D=M");
+    write`@${inlineTable[location + offset]}
+          D=M`;
   } else if (location == "constant") {
-    output.push("@" + offset);
-    output.push("D=A");
+    write`@${offset}
+          D=A`;
   } else if (location == "static") {
-    output.push(`@${fileName}.${offset}`);
-    output.push("D=M");
+    write`@${fileName}.${offset}
+          D=M`;
   } else {
-    output.push("@" + table[location]);
-    output.push("D=M");
-    output.push("@" + offset);
-    output.push("A=D+A");
-    output.push("D=M");
+    write`@${table[location]}
+          D=M
+          @${offset}
+          A=D+A
+          D=M`;
   }
-  output.push("@SP");
-  output.push("M=M+1");
-  output.push("A=M-1");
-  output.push("M=D");
+  write`@SP
+        M=M+1
+        A=M-1
+        M=D`;
 }
 
 function writePop(location: string, offset: string) {
@@ -67,71 +77,71 @@ function writePop(location: string, offset: string) {
   let normalCase = ["local", "argument", "this", "that"].includes(location);
 
   if (normalCase) {
-    output.push("@" + table[location]);
-    output.push("D=M");
-    output.push("@" + offset);
-    output.push("D=D+A");
-    output.push("@R13");
-    output.push("M=D");
+    write`@${table[location]}
+          D=M
+          @${offset}
+          D=D+A
+          @R13
+          M=D`;
   }
 
-  output.push("@SP");
-  output.push("AM=M-1");
-  output.push("D=M");
+  write`@SP
+        AM=M-1
+        D=M`;
 
   if (normalCase) {
-    output.push("@R13");
-    output.push("A=M");
+    write`@R13
+          A=M`;
   } else {
     if (location == "static") {
-      output.push(`@${fileName}.${offset}`);
+      write`@${fileName}.${offset}`;
     } else {
-      output.push("@" + inlineTable[location + offset]);
+      write`@${inlineTable[location + offset]}`;
     }
   }
 
-  output.push("M=D");
+  write`M=D`;
 }
 
 function writeArithmetic(type: string) {
   const arithmeticTable = { add: "+", sub: "-", and: "&", or: "|" };
-  output.push("@SP");
-  output.push("AM=M-1");
-  output.push("D=M");
-  output.push("A=A-1");
-  output.push(`M=M${arithmeticTable[type]}D`);
+  write`@SP
+        AM=M-1
+        D=M
+        A=A-1
+        M=M${arithmeticTable[type]}D`;
 }
 
 function writeBit(type: string) {
   const bitTable = { neg: "-", not: "!" };
-  output.push("@SP");
-  output.push("A=M-1");
-  output.push(`M=${bitTable[type]}M`);
+  write`@SP
+        A=M-1
+        M=${bitTable[type]}M`;
+}
+
+
+function writeBoolean(type: string, counter: number) {
+  const booleanTable = { eq: "JEQ", lt: "JLT", gt: "JGT" };
+  write`@SP
+        AM=M-1
+        D=M
+        A=A-1
+        D=M-D
+        M=0
+        @TRUTHY.${counter}
+        D;${booleanTable[type]}
+        @DONE.${counter}
+        0;JMP
+        (TRUTHY.${counter})
+        @SP
+        A=M-1
+        M=-1
+        (DONE.${counter})`;
 }
 
 let counter = 0;
-function writeBoolean(type: string) {
-  const booleanTable = { eq: "JEQ", lt: "JLT", gt: "JGT" };
-  output.push("@SP");
-  output.push("AM=M-1");
-  output.push("D=M");
-  output.push("A=A-1");
-  output.push("D=M-D");
-  output.push("M=0");
-  output.push("@TRUTHY." + counter);
-  output.push(`D;${booleanTable[type]}`);
-  output.push("@DONE." + counter);
-  output.push("0;JMP");
-  output.push("(TRUTHY." + counter + ")");
-  output.push("@SP");
-  output.push("A=M-1");
-  output.push("M=-1");
-  output.push("(DONE." + counter + ")");
-  counter++;
-}
-
 for (const line of lines) {
-  let [type, location, offset] = line.split(/ +/g);
+  let [type, location, offset] = line.split(/\s+/g);
   if ("push" == type) {
     writePush(location, offset);
   } else if ("pop" == type) {
@@ -141,9 +151,10 @@ for (const line of lines) {
   } else if (["neg", "not"].includes(type)) {
     writeBit(type);
   } else if (["eq", "lt", "gt"].includes(type)) {
-    writeBoolean(type);
+    writeBoolean(type, counter);
+    counter++;
   } else {
-    console.log("UNKNOWN", line);
+    throw new Error(`Unknown command: ${line}`);
   }
 }
 
