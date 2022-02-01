2b16efda585eb2796129c5a939fcc9de234f0ce6
modderme123 <modderme123@gmail.com>
2022-01-31 20:26:57 -0800

    more work

--- projects/08/translator/app/Main.hs
+++ projects/08/translator/app/Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
-import BasicPrelude hiding (replicate)
+import BasicPrelude
 import Control.Monad.State (State, evalState, get, put)
-import Data.Text (pack, strip, unpack, replicate)
+import Data.Text (pack, strip, unpack)
 import qualified Data.Text as T (takeWhile)
 import Data.Text.Read (decimal)
 import System.FilePath (replaceExtension, takeBaseName)
@@ -45,7 +45,6 @@ logic fileName str = do
 pushBody = "@SP\nM=M+1\nA=M-1\nM=D"
 
 push :: Text -> Text -> Text -> Text
-push _ "constant" "0" = "@SP\nM=M+1\nA=M-1\nM=0"
 push _ "constant" offset = "@" ++ offset ++ "\nD=A\n" ++ pushBody
 push _ "pointer" "0" = "@THIS\nD=M\n" ++ pushBody
 push _ "pointer" "1" = "@THAT\nD=M\n" ++ pushBody
@@ -85,18 +84,19 @@ ifgoto name = do
   state <- get
   return $ "@SP\nAM=M-1\nD=M\n@" ++ (functionName state) ++ "$" ++ name ++ "\nD;JNE"
 
-
 parseArgs :: Text -> Int
 parseArgs args =
   case decimal args of
     Right (b, _) -> b
     _ -> error (unpack $ "Uknown args: " ++ args)
-  
+
 functionT :: Text -> Text -> State MyState Text
 functionT name n = do
   state <- get
   put state {functionName = name}
-  return $ replicate (parseArgs n) (push "" "constant" "0")
+  return $
+    "(" ++ name ++ ")\n"
+      ++ (intercalate "\n" $ replicate (parseArgs n) ("@SP\nM=M+1\nA=M-1\nM=0"))
 
 callT :: Text -> Text -> State MyState Text
 callT name n = do
@@ -104,32 +104,31 @@ callT name n = do
   let mycount = tshow $ counter state
   put state {counter = counter state + 1}
   return $
-    intercalate "\n"
+    intercalate
+      "\n"
       [ "@RETURN." ++ mycount,
-        "@LCL\nD=M\n@SP\nA=M\nM=D",
-        "@ARG\nD=M\n@SP\nA=M\nM=D",
-        "@THIS\nD=M\n@SP\nA=M\nM=D",
-        "@THAT\nD=M\n@SP\nA=M\nM=D",
-        "@SP\nD=M\n@LCL\nM=D\n@" ++ n ++ "\nD=D-A\n@ARG\nM=D",
+        "D=A\n@SP\nM=M+1\nA=M-1\nM=D",
+        "@LCL\nD=M\n@SP\nM=M+1\nA=M-1\nM=D",
+        "@ARG\nD=M\n@SP\nM=M+1\nA=M-1\nM=D",
+        "@THIS\nD=M\n@SP\nM=M+1\nA=M-1\nM=D",
+        "@THAT\nD=M\n@SP\nM=M+1\nA=M-1\nM=D",
+        "@SP\nD=M\n@LCL\nM=D\n@" ++ n ++ "\nD=D-A\n@5\nD=D-A\n@ARG\nM=D",
         "@" ++ name,
         "0;JMP",
         "(RETURN." ++ mycount ++ ")"
       ]
 
 returnT :: State MyState Text
-returnT = do
-  state <- get
+returnT =
   return $
-    intercalate "\n"
-      [ "@LCL\nD=M\n@R13\nM=D",
-        "@5\nD=A\n@R13\nA=M-D\nD=M\n@ARG\nM=D",
-        "@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D",
-        "@R13\nM=M-1\nA=M\nD=M\n@THAT\nM=D",
-        "@R13\nM=M-1\nA=M\nD=M\n@THIS\nM=D",
-        "@R13\nM=M-1\nA=M\nD=M\n@ARG\nM=D",
-        "@R13\nM=M-1\nA=M\nD=M\n@LCL\nM=D",
-        "A=M\n0;JMP"
-      ]
+    "@LCL\nD=M\n@5\nA=D-A\nD=M\n@R13\nM=D\n" -- TODO: Why can't this go two lines down?
+      ++ "@SP\nA=M-1\nD=M\n@ARG\nA=M\nM=D\n"
+      ++ "D=A+1\n@SP\nM=D\n"
+      ++ "@LCL\nAM=M-1\nD=M\n@THAT\nM=D\n"
+      ++ "@LCL\nAM=M-1\nD=M\n@THIS\nM=D\n"
+      ++ "@LCL\nAM=M-1\nD=M\n@ARG\nM=D\n"
+      ++ "@LCL\nA=M-1\nD=M\n@LCL\nM=D\n"
+      ++ "@R13\nA=M\n0;JMP\n"
 
 processItem :: Text -> [Text] -> State MyState Text
 processItem fileName ["push", location, dest] = return $ push fileName location dest
@@ -148,10 +147,14 @@ processItem _ ["goto", name] = gotoT name
 processItem _ ["if-goto", name] = ifgoto name
 processItem _ ["function", name, n] = functionT name n
 processItem _ ["call", name, n] = callT name n
+processItem _ ["return"] = returnT
 processItem _ a = error (unpack $ "Unknown command: " ++ unwords a)
 
 parseFile :: Text -> [Text]
 parseFile = filter (/= "") . map (strip . T.takeWhile (/= '/')) . lines
+
+comment :: Text -> Text
+comment x = "//" ++ x
 
 main :: IO ()
 main = do
@@ -162,6 +165,8 @@ main = do
   let fileLines = parseFile file
   let parser = processItem (pack $ takeBaseName fileName) . words
   let initialState = MyState {counter = 0, functionName = ""}
-  let output = unlines $ evalState (mapM parser fileLines) initialState
+  let o1 = evalState (mapM parser fileLines) initialState
+  let o2 = map comment fileLines
+  let output = unlines $ concat (transpose [o2, o1])
 
   writeFile (replaceExtension fileName "asm") output
